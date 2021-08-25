5b0717f3e33849e0872bab2faa4fa59830a38b53
Crowdbotics <team@crowdbotics.com>
2021-08-25 16:46:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lovelywar_30098Identifier",
+  url: "https://lovelywar_30098Identifier.botics.co",
+  api: "https://lovelywar_30098Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
